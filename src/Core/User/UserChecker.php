2df8ec20f4679cbbe60ddfc5e64136b64689bb6e
Kamil Kokot <kamil@kokot.me>
2021-01-12 19:02:38 +0100

    Add polyfill for UserChecker

---
+++ src/Core/User/UserChecker.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SyliusLabs\Polyfill\Symfony\Security\Core\User;
+
+use Symfony\Component\Security\Core\Exception\AccountExpiredException;
+use Symfony\Component\Security\Core\Exception\CredentialsExpiredException;
+use Symfony\Component\Security\Core\Exception\DisabledException;
+use Symfony\Component\Security\Core\Exception\LockedException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @link https://github.com/symfony/symfony/blob/v4.4.18/src/Symfony/Component/Security/Core/User/UserChecker.php
+ */
+final class UserChecker implements UserCheckerInterface
+{
+    /** @var UserCheckerInterface */
+    private $userChecker;
+
+    public function __construct(UserCheckerInterface $userChecker)
+    {
+        $this->userChecker = $userChecker;
+    }
+
+    public function checkPreAuth(UserInterface $user): void
+    {
+        $this->userChecker->checkPreAuth($user);
+
+        if (!$user instanceof AdvancedUserInterface) {
+            return;
+        }
+
+        if (!$user->isAccountNonLocked()) {
+            $ex = new LockedException('User account is locked.');
+            $ex->setUser($user);
+            throw $ex;
+        }
+
+        if (!$user->isEnabled()) {
+            $ex = new DisabledException('User account is disabled.');
+            $ex->setUser($user);
+            throw $ex;
+        }
+
+        if (!$user->isAccountNonExpired()) {
+            $ex = new AccountExpiredException('User account has expired.');
+            $ex->setUser($user);
+            throw $ex;
+        }
+    }
+
+    public function checkPostAuth(UserInterface $user): void
+    {
+        $this->userChecker->checkPostAuth($user);
+
+        if (!$user instanceof AdvancedUserInterface) {
+            return;
+        }
+
+        if (!$user->isCredentialsNonExpired()) {
+            $ex = new CredentialsExpiredException('User credentials have expired.');
+            $ex->setUser($user);
+            throw $ex;
+        }
+    }
+}
---
+++ tests/Core/User/UserCheckerTest.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\SyliusLabs\Polyfill\Symfony\Security\Core\User;
+
+use PHPUnit\Framework\TestCase;
+use SyliusLabs\Polyfill\Symfony\Security\Core\User\AdvancedUserInterface;
+use SyliusLabs\Polyfill\Symfony\Security\Core\User\UserChecker;
+use Symfony\Component\Security\Core\Exception\AccountExpiredException;
+use Symfony\Component\Security\Core\Exception\CredentialsExpiredException;
+use Symfony\Component\Security\Core\Exception\DisabledException;
+use Symfony\Component\Security\Core\Exception\LockedException;
+use Symfony\Component\Security\Core\User\UserChecker as SymfonyUserChecker;
+
+final class UserCheckerTest extends TestCase
+{
+    /** @var UserChecker */
+    private $userChecker;
+
+    protected function setUp(): void
+    {
+        $this->userChecker = new UserChecker(new SymfonyUserChecker());
+    }
+
+    /** @test */
+    public function it_throws_locked_exception_if_account_is_locked(): void
+    {
+        $this->expectException(LockedException::class);
+
+        $user = $this->createMock(AdvancedUserInterface::class);
+        $user->method('isAccountNonLocked')->willReturn(false);
+        $user->method('isEnabled')->willReturn(true);
+        $user->method('isAccountNonExpired')->willReturn(true);
+        $user->method('isCredentialsNonExpired')->willReturn(true);
+
+        $this->userChecker->checkPreAuth($user);
+        $this->userChecker->checkPostAuth($user);
+    }
+
+    /** @test */
+    public function it_throws_disabled_exception_if_account_is_disabled(): void
+    {
+        $this->expectException(DisabledException::class);
+
+        $user = $this->createMock(AdvancedUserInterface::class);
+        $user->method('isAccountNonLocked')->willReturn(true);
+        $user->method('isEnabled')->willReturn(false);
+        $user->method('isAccountNonExpired')->willReturn(true);
+        $user->method('isCredentialsNonExpired')->willReturn(true);
+
+        $this->userChecker->checkPreAuth($user);
+        $this->userChecker->checkPostAuth($user);
+    }
+
+    /** @test */
+    public function it_throws_account_expired_exception_if_account_is_expired(): void
+    {
+        $this->expectException(AccountExpiredException::class);
+
+        $user = $this->createMock(AdvancedUserInterface::class);
+        $user->method('isAccountNonLocked')->willReturn(true);
+        $user->method('isEnabled')->willReturn(true);
+        $user->method('isAccountNonExpired')->willReturn(false);
+        $user->method('isCredentialsNonExpired')->willReturn(true);
+
+        $this->userChecker->checkPreAuth($user);
+        $this->userChecker->checkPostAuth($user);
+    }
+
+    /** @test */
+    public function it_throws_credentials_expired_exception_if_credentials_are_expired(): void
+    {
+        $this->expectException(CredentialsExpiredException::class);
+
+        $user = $this->createMock(AdvancedUserInterface::class);
+        $user->method('isAccountNonLocked')->willReturn(true);
+        $user->method('isEnabled')->willReturn(true);
+        $user->method('isAccountNonExpired')->willReturn(true);
+        $user->method('isCredentialsNonExpired')->willReturn(false);
+
+        $this->userChecker->checkPreAuth($user);
+        $this->userChecker->checkPostAuth($user);
+    }
+}
